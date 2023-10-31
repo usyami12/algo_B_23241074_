f943be25a2656c81f72c92752fe49e8d56bbf537
usyami12 <146051216+usyami12@users.noreply.github.com>
2023-10-31 13:19:26 +0800

    kuliah algo 31 oktober 2023

---
+++ 01 programjavanapertama/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // program biodata mahasiswa
+        // deklarasi variable
+    String nama;
+    String nim;   
+    String tgl_lahir; 
+    String alamat;
+    float berat_badan;
+    float tinggi_badan; 
+
+    // mengisi variable
+    nama ="selki debei usyami";
+    nim = "23241074"
+    tgl_lahir =" 2 desember 2003"
+    alamat ="sumbawah barat"
+    berat_badan = 42.3f;
+    tinggi_badan = 142.4f;
+
+    // cetak badan 
+    System.out. printin("===============");
+    System.out. println("Nama : "+ nama);
+    System.out.println("NIM: "nim);
+    System.out.println ("tanggal lahir : "+ tgl_lahir);
+    System.out.println("alamat : " + alamat);
+    System.out.println("berat badan : " + berat_badan);
+    System.out.println("tinggi badan : " + tinggi_badan);
+    System.out.println("=====================");
